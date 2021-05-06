cda803d1f92d45e6f00ecc39748fd8e6996b6da5
Liuser <614194188@qq.com>
2021-05-06 12:04:40 +0800

    默认变更列表

---
+++ game/high_energy_bullet.py
@@ -0,0 +1,43 @@
+import  pygame
+
+from pygame.sprite import Sprite
+
+
+class Highbullet(Sprite):
+    def __init__(self, ai_settings, screen):
+        # 初始化外星人并设置其起始位置
+        super(Highbullet, self).__init__()
+        self.screen = screen
+        self.ai_setting=ai_settings
+        self.image = pygame.image.load('h1.bmp')
+        self.rect = self.image.get_rect()
+        self.rect.x=self.screen.get_rect().centerx
+        #self.rect.y=self.screen.get_rect().top-100  # 特效子弹起始位置
+        self.yy=float(self.rect.y)
+        self.speed=ai_settings.super_bullet_speed
+        self.number=0
+
+
+
+    def update(self):
+
+            self.yy+= self.speed  #单纯的数字加
+
+            self.rect.y = self.yy    #让坐标的值等于数字
+
+
+    def stop(self):
+        self.rect.x=2000
+        self.rect.y=self.yy+800
+
+
+
+
+    def blitme(self):
+        self.screen.blit(self.image,self.rect)  #blit 块传送   把这个图像传到 self.rect制定的位置
+
+
+
+
+
+
